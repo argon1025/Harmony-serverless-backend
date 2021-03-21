cfd7d590db84c080a9c901e4bb1371d91baeedb5
argon1025 <argon1025@gmail.com>
2021-03-22 02:08:14 +0900

    Add 로깅 모듈내 모든 이벤트를 전체출력하는 옵션을 추가했습니다

--- service/saveLogs.js
+++ service/saveLogs.js
@@ -1,5 +1,5 @@
-//로그를 발생합니다
-async function saveLogs(event,detail="") {
+//로그생성 서비스
+async function saveLogs(event,detail="",fullLogs=false) {
     // 타겟 아이피
     const TARGET_ADDRESS = event.requestContext.identity.sourceIp;
     //요청 URL
@@ -9,5 +9,10 @@ async function saveLogs(event,detail="") {
 
     //해당양식에 맞게 기록
     console.log(`[system] ${METHOD} ${TARGET_ADDRESS} - ${RESOURCE} - detail[${detail}]`);
+
+    //테스트용 전체 이벤트로그 출력
+    if(fullLogs){
+        console.log(event);
+    }
 }
 module.exports = saveLogs;
