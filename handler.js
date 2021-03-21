25c5cedd603a1c7fd40da0b7165649374a29140e
argon1025 <argon1025@gmail.com>
2021-03-22 02:07:21 +0900

    Modify accounts.hello endpoint 삭제 및 주석 추가

--- handler.js
+++ handler.js
@@ -11,7 +11,8 @@ module.exports.hello = controllers.accounts.hello;
  * accounts Handler
  */
 module.exports.accounts = {
+    // POST /api/v1/kakao/account 회원등록을 요청
     kakaoAcount: controllers.accounts.kakaoAcount,
+    // GET /api/v1/kakao/authorize 로그인을 요청
     kakaoAuthorize: controllers.accounts.kakaoAuthorize,
-    hello: controllers.accounts.hello,
 };
