869b0c186011998eb11b96977790185429d3095e
argon1025 <argon1025@gmail.com>
2021-03-22 02:08:59 +0900

    Add ERROR_TABLE 객체를 참조하여 에러응답을 자동으로 생성하는 메서드 추가

--- service/Response.js
+++ service/Response.js
@@ -1,4 +1,19 @@
+//에러일경우, 메시지, 코드
 class Response {
+    constructor() {
+        this.ERROR_TABLE = {
+            "default Error": {
+                statusCode: "500",
+                errorCode: "0",
+                errorMessage: "Server logic error",
+            },
+            "Bodydata does not exist": {
+                statusCode: "404",
+                errorCode: "1",
+                errorMessage: "Bodydata does not exist",
+            },
+        };
+    }
     async nomalResponse(statusCode = 200, header = {}, body = {}) {
         const response = {
             statusCode: statusCode,
@@ -7,7 +22,12 @@ class Response {
         return response;
     }
 
-    async errorResponse(statusCode = 401, header = {}, errorCode = "", errorMsg = "") {
+    async errorResponse(
+        statusCode = 401,
+        header = {},
+        errorCode = "",
+        errorMsg = ""
+    ) {
         //응답 데이터를 작성합니다
         const response = {
             statusCode: statusCode,
@@ -17,6 +37,49 @@ class Response {
                 2
             ),
         };
+
+        return response;
+    }
+
+    async errorResponseUseErrorTable(error) {
+        let response;
+
+        // ERROR_TABLE내 데이터 존재 유무 체크
+        if (this.ERROR_TABLE[error.message]) {
+            // 에러 객체 지정
+            let errorInfo = this.ERROR_TABLE[error.message];
+
+            // 지정된 에러 객체로 응답 작성
+            response = {
+                statusCode: errorInfo.statusCode,
+                body: JSON.stringify(
+                    {
+                        errorCode: errorInfo.errorCode,
+                        msg: errorInfo.errorMessage,
+                    },
+                    null,
+                    2
+                ),
+            };
+        } else {
+            // ERROR_TABLE내 참조할 데이터가 없을경우 default Error로 응답
+
+            // 에러 객체 지정
+            let errorInfo = this.ERROR_TABLE["default Error"];
+
+            // 저장된 에러 객체로 응답 작성
+            response = {
+                statusCode: errorInfo.statusCode,
+                body: JSON.stringify(
+                    {
+                        errorCode: errorInfo.errorCode,
+                        msg: errorInfo.errorMessage,
+                    },
+                    null,
+                    2
+                ),
+            };
+        }
 
         return response;
     }
