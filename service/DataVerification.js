1a95414d20b3627e41e123023f851163f4601ab6
argon1025 <argon1025@gmail.com>
2021-03-26 16:49:34 +0900

    Add Number 데이터 검증 메서드 추가

--- service/DataVerification.js
+++ service/DataVerification.js
@@ -1,20 +1,35 @@
+/**
+ * DataVerification
+ * 2021.03.26
+ * 
+ * Joi 라이브러리 사용
+ * https://joi.dev/api/?v=17.4.0
+ */
 const Joi = require("joi");
 
 class DataVerification {
     constructor() {
+        this.MAX_SAFE_INT = Number.MAX_SAFE_INTEGER;
         this.URL_PATTERN = new RegExp(`^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z|A-Z|0-9|:|.|_|/]+$`);
         this.JOBTAG_PATTERN = new RegExp(`^[0-9]+$`);
 
-        //URL 데이터
+        // 스키마 설정
+
+        // URL 데이터
         this.URL_SCHEMA = Joi.object({
             url: Joi.string().pattern(this.URL_PATTERN).min(0).max(99),
         });
-
+        // JobTag 데이터
         this.JOBTAG_SCHEMA = Joi.object({
             jobtag: Joi.string().pattern(this.JOBTAG_PATTERN).min(0).max(99),
         });
+        // 단순 숫자 데이터
+        this.NUMBER_SCHEMA = Joi.object({
+            number: Joi.number().min(0).max(this.MAX_SAFE_INT),
+        });
     }
 
+    // URL 데이터
     async checkURL(value) {
         const result = await this.URL_SCHEMA.validate({ url: value });
 
@@ -27,8 +42,20 @@ class DataVerification {
         }
     }
 
+    // JobTag 데이터
     async checkJobtag(value) {
         const result = await this.JOBTAG_SCHEMA.validate({ jobtag: value });
+
+        if (!result.error) {
+            return result;
+        } else {
+            throw new Error("Value verification failed");
+        }
+    }
+
+    // 단순 숫자 데이터
+    async checkNumber(value){
+        const result = await this.NUMBER_SCHEMA.validate({number:value});
 
         if (!result.error) {
             return result;
