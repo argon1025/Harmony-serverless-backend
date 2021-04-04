68c06920db39482241acc8f6cd4e6b889500838c
argon1025 <argon1025@gmail.com>
2021-04-04 21:07:21 +0900

    Delete Test.hello EndPoint를 삭제했습니다

--- handler.js
+++ handler.js
@@ -2,11 +2,6 @@
 const controllers = require("./controllers");
 
 /**
- * demo Handler
- */
-module.exports.hello = controllers.accounts.hello;
-
-/**
  * accounts Handler
  */
 module.exports.accounts = {
@@ -20,11 +15,17 @@ module.exports.accounts = {
     modifyAccount: controllers.accounts.modifyAccount,
 };
 
+/**
+ * jobTags Handler
+ */
 module.exports.jobTags = {
     // GET /api/v1/jobtags 잡 태그 리스트
     jobTagList: controllers.jobtags.jobTagList,
 };
 
+/**
+ * projects Handler
+ */
 module.exports.projects = {
     // GET /api/v1/projects 프로젝트 리스트
     projectList: controllers.projects.projectList,
